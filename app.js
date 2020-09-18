3a51d91ef30fb6cf9d61e0718cfc119a20e5321c
dharmyx <teminidami@yahoo.co.uk>
2020-09-18 21:04:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function initializeClock(id, endtime) {
   const timeinterval = setInterval(updateClock, 1000);
 }
 
-const deadline = new Date(Date.parse(new Date()) + 1 * 22 * 60 * 60 * 1000);
+const deadline = new Date(Date.parse(new Date("Sept 18, 2020 8:00:00")) + 1 * 24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
 
 
